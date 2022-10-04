3de9925c46c927c39ce790438ee22f4fca14b340
katsm <k.smiglak.marczyk@gmail.com>
2022-10-04 23:30:51 +0200

    use commandline arguments

--- script/test.js
+++ script/test.js
@@ -1,9 +1,24 @@
-const newman = require('newman'); 
+const newman = require('newman');
+
+const args = process.argv;
+let testcase = "";
+
+switch (args[2]) {
+    case 'upload':
+        testcase = "Upload test";
+        break;
+    case 'delete':
+        testcase = "Delete test";
+        break;
+    case 'metadata':
+        testcase = "Get File Metadata test";
+        break;
+}
 
 newman.run({
     collection: require('../postman/DropboxAPITest.postman_collection.json'),
     environment: require('../postman/DropboxEnv.postman_environment.json'),
-    folder: 'Delete test',
+    folder: testcase,
     reporters: ['cli', 'htmlextra'],
     reporter: {
         htmlextra: {
@@ -14,5 +29,4 @@ newman.run({
     }
 }, function (err) {
     if (err) { throw err; }
-    console.log('collection run complete!');
 });
