c3f91124a73e98b98c9fe6133d3a176be6e0a25a
katsm <k.smiglak.marczyk@gmail.com>
2022-10-06 23:13:30 +0200

    reorganise the code

--- script/test.js
+++ script/test.js
@@ -1,32 +1,42 @@
 const newman = require('newman');
 
-const args = process.argv;
-let testcase = "";
-
-switch (args[2]) {
-    case 'upload':
-        testcase = "Upload test";
-        break;
-    case 'delete':
-        testcase = "Delete test";
-        break;
-    case 'metadata':
-        testcase = "Get File Metadata test";
-        break;
+const nm = function (testCase) {
+    newman.run({
+        collection: require('../postman/DropboxAPITest.postman_collection.json'),
+        environment: require('../postman/DropboxEnv.postman_environment.json'),
+        folder: testCase,
+        reporters: ['cli', 'htmlextra'],
+        reporter: {
+            htmlextra: {
+                export: './report/report.html',
+                logs: true,
+                displayProgressBar: true
+            }
+        }
+    }, function (err) {
+        if (err) { throw err; }
+    });
 }
 
-newman.run({
-    collection: require('../postman/DropboxAPITest.postman_collection.json'),
-    environment: require('../postman/DropboxEnv.postman_environment.json'),
-    folder: testcase,
-    reporters: ['cli', 'htmlextra'],
-    reporter: {
-        htmlextra: {
-            export: './report/report.html',
-            logs: true,
-            displayProgressBar: true
-        }
+const getFolder = function (opt) {
+    let folder;
+    switch (opt) {
+        case 'upload':
+            folder = "Upload test";
+            break;
+        case 'delete':
+            folder = "Delete test";
+            break;
+        case 'metadata':
+            folder = "Get File Metadata test";
+            break;
+        default:
+            folder ="";
+            break;
     }
-}, function (err) {
-    if (err) { throw err; }
-});
+    return folder;
+}
+
+const args = process.argv;
+const folderToRun = getFolder(args[2]);
+nm(folderToRun);
